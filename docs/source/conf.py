f9794ff6c846d6ebef121f6d18ac79458b789ed1
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2024-12-05 17:22:21 +0200

    Fixed copyright + version bump
    Fixed some copyright strings to correctly give credit to the original
    owners and bumped the version.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,9 +12,9 @@ sys.path.insert(0, os.path.abspath('../../'))
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'Zephyr Python API'
-copyright = '2024, Petr Sharapenko'
-author = 'Petr Sharapenko'
-release = '0.1.0'
+copyright = ['2022-2024, Petr Sharapenko', '2024, Viorel-Catalin Rapiteanu']
+author = 'Viorel-Catalin Rapiteanu'
+release = '0.1.1'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
