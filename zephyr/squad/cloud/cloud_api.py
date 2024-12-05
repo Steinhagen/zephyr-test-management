947986776672d8efa1d1ea3994aa1f6bbe0c3611
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2024-12-05 22:15:16 +0200

    Initial work to get Zephyr Squad Cloud working

---
+++ zephyr/squad/cloud/cloud_api.py
@@ -0,0 +1,74 @@
+"""
+A module with the Zephyr Squad Cloud Api wrapper class.
+"""
+import logging
+from zephyr.squad.zephyr_squad_session import ZephyrSquadSession
+from zephyr.squad.cloud import endpoints
+
+# pylint: disable=missing-function-docstring
+class CloudApiWrapper:
+    """
+    Zephyr Squad Cloud Api wrapper class. It contains API endpoint wrappers for the Zephyr Squad
+    Cloud. The wrappers are grouped by the entity they are related to.
+    These wrapper groups are represented by the properties of the class.
+
+    For more details on the API endpoints see docs:
+    https://support.smartbear.com/zephyr-scale-cloud/api-docs/
+
+    :param session: ZephyrSession object with auth credentials
+    """
+    def __init__(self, session: ZephyrSquadSession):
+        self.session = session
+        self.logger = logging.getLogger(__name__)
+
+    # @property
+    # def test_cases(self):
+    #     return endpoints.TestCaseEndpoints(self.session)
+
+    # @property
+    # def test_cycles(self):
+    #     return endpoints.TestCycleEndpoints(self.session)
+
+    # @property
+    # def test_plans(self):
+    #     return endpoints.TestPlanEndpoints(self.session)
+
+    # @property
+    # def test_executions(self):
+    #     return endpoints.TestExecutionEndpoints(self.session)
+
+    # @property
+    # def folders(self):
+    #     return endpoints.FolderEndpoints(self.session)
+
+    # @property
+    # def statuses(self):
+    #     return endpoints.StatusEndpoints(self.session)
+
+    # @property
+    # def priorities(self):
+    #     return endpoints.PriorityEndpoints(self.session)
+
+    # @property
+    # def environments(self):
+    #     return endpoints.EnvironmentEndpoints(self.session)
+
+    # @property
+    # def projects(self):
+    #     return endpoints.ProjectEndpoints(self.session)
+
+    # @property
+    # def links(self):
+    #     return endpoints.LinkEndpoints(self.session)
+
+    # @property
+    # def issue_links(self):
+    #     return endpoints.IssueLinksEndpoints(self.session)
+
+    # @property
+    # def automations(self):
+    #     return endpoints.AutomationEndpoints(self.session)
+
+    # @property
+    # def healthcheck(self):
+    #     return endpoints.HealthcheckEndpoints(self.session)
--- zephyr/squad/squad.py
+++ zephyr/squad/squad.py
@@ -1,26 +1,46 @@
+"""
+A module with the Zephyr Squad base object.
+"""
+
 import logging
+from sys import api_version
 
 from zephyr.squad.zephyr_squad_session import ZephyrSquadSession
+from zephyr.squad.cloud.cloud_api import CloudApiWrapper
 from zephyr.squad.server.server_api import ServerApiWrapper
 from zephyr.squad.server.actions import ServerActionsWrapper
 
-DEFAULT_BASE_URL = "https://jira.hosted.com/"
+DEFAULT_BASE_URL = "https://prod-api.zephyr4jiracloud.com/v2/"
 
+API_V2 = "v2"
+API_V1 = "v1"
 
 class ZephyrSquad:
     """
-    Zephyr Squad base object to interact with other objects or raw api by its methods.
+    Zephyr Squad base object to interact with other objects or raw api
+    by its methods. You should define the API version of Zephyr Squad instance
+    you are going to work with. Server is v1 and Cloud is v2.
 
-    :param base_url: base API url to connect with
+    :param base_url: base API url to connect with. An example for
+                     Squad Server looks like 'https://jira.hosted.com'
+    :param api_version: 'v2' for Cloud and 'v1' for Server
+
+    :raises ValueError: if api_version is not 'v1' or 'v2'
     """
-    def __init__(self, base_url=None, **kwargs):
+    def __init__(self, base_url=None, api_version=API_V2, **kwargs):
         base_url = DEFAULT_BASE_URL if not base_url else base_url
         session = ZephyrSquadSession(base_url=base_url, **kwargs)
-        self.api = ServerApiWrapper(session)
-        self.actions = ServerActionsWrapper(session)
+
+        if api_version.lower() == API_V2:
+            self.api = CloudApiWrapper(session)
+        elif api_version.lower() == API_V1:
+            self.api = ServerApiWrapper(session)
+            self.actions = ServerActionsWrapper(session)
+        else:
+            raise ValueError("API version should be either 'v1' (Server) or 'v2' (Cloud)")
         self.logger = logging.getLogger(__name__)
 
     @classmethod
     def server_api(cls, base_url, **kwargs):
         """Alternative constructor for Zephyr Squad Server client"""
-        return cls(base_url=base_url, **kwargs)
+        return cls(base_url=base_url, api_version=API_V1, **kwargs)
