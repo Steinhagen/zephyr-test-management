c7d54d62dac627dc546408e6de2a138bf80af605
Viorel-Cătălin Răpițeanu <rapiteanu.catalin@gmail.com>
2024-12-06 09:38:40 +0200

    Added test create issue and web links functions

--- zephyr/squad/cloud/endpoints/endpoints.py
+++ zephyr/squad/cloud/endpoints/endpoints.py
@@ -98,30 +98,18 @@ class TestCaseEndpoints(EndpointTemplate):
         """Returns links for a test case with specified key."""
         return self.session.get(Paths.CASE_LINKS.format(test_case_key))
 
-    # def create_issue_links(self, test_case_key: str, issue_id: int):
-    #     """Creates a link between a test case and a Jira issue"""
-    #     json = {"issueId": issue_id}
-    #     return self.session.post(Paths.CASE_ISSUES.format(test_case_key),
-    #                              json=json)
+    def create_issue_links(self, test_case_key: str, issue_id: int):
+        """Creates a link between a test case and a Jira issue."""
+        json = {"issueId": issue_id}
+        return self.session.post(Paths.CASE_ISSUES.format(test_case_key),
+                                 json=json)
 
-    # def create_web_links(self, test_case_key: str, url: str, **kwargs):
-    #     """Creates a link between a test case and a generic URL"""
-    #     json = {"url": url}
-    #     json.update(kwargs)
-    #     return self.session.post(Paths.CASE_WEBLINKS.format(test_case_key),
-    #                              json=json)
-
-    # def get_versions(self, test_case_key: str, **kwargs):
-    #     """
-    #     Returns all test case versions for a test case with specified key.
-    #     Response is ordered by most recent first
-    #     """
-    #     return self.session.get_paginated(Paths.CASE_VERS.format(test_case_key),
-    #                                       params=kwargs)
-
-    # def get_version(self, test_case_key: str, version: str):
-    #     """Retrieves a specific version of a test case"""
-    #     return self.session.get(Paths.CASE_VER.format(test_case_key, version))
+    def create_web_links(self, test_case_key: str, url: str, **kwargs):
+        """Creates a link between a test case and a generic URL"""
+        json = {"url": url}
+        json.update(kwargs)
+        return self.session.post(Paths.CASE_WEBLINKS.format(test_case_key),
+                                 json=json)
 
     # def get_test_script(self, test_case_key: str):
     #     """Returns the test script for the given test case"""
